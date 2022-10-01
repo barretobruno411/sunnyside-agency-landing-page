1c68ccce57add79e6f903d55034a9f8b01ff6294
Bruno Duarte <bruno.duarte411@gmail.com>
2022-10-01 00:58:01 -0300

    Add mobile menu design and hidden

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,14 @@ const Navigation = () => {
   return (
     <nav>
         <h1 className="logo">sunnyside</h1>
+        
         <img src={hamburger} alt="hamburger-menu" className='hamburger-menu'/>
+        <ul className='mobile-menu hidden'>
+          <li>About</li>
+          <li>Services</li>
+          <li>Projects</li>
+          <li className='contact'>Contact</li>
+        </ul>
     </nav>
   )
 }
