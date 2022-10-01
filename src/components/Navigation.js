e9efa4a60326d640497f833381ea4d279b68d05c
Bruno Duarte <bruno.duarte411@gmail.com>
2022-09-30 22:10:09 -0300

    Add navigation style

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import "./Navigation.css"
+import hamburger from "../images/icon-hamburger.svg"
 
 const Navigation = () => {
   return (
-    <div>Navigation</div>
+    <nav>
+        <h1 className="logo">sunnyside</h1>
+        <img src={hamburger} alt="hamburger-menu" className='hamburger-menu'/>
+    </nav>
   )
 }
 
