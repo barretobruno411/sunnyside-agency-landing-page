b6e9277961c6cff2e70d69147b1a132b1413aa0a
Bruno Duarte <bruno.duarte411@gmail.com>
2022-10-03 16:18:58 -0300

    Add article graphic mobile design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from "./components/Header"
 import Main from "./components/Main"
+import ArticleGraphic from "./components/ArticleGraphic"
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <ArticleGraphic />
       
     </div>
   );
---
+++ src/components/ArticleGraphic.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./ArticleGraphic.css";
+
+const ArticleGraphic = () => {
+  return (
+    <article className="articleGraphic">
+      <div className="graphic design">
+        <h2>Graphic Design</h2>
+        <p>
+          Great design makes you memorable. We deliver artwork that underscores
+          your brand message and capture potential client's attention.
+        </p>
+      </div>
+      <div className="graphic photography">
+        <h2>Photography</h2>
+        <p>
+          Increace your credibility by getting the most stunning high-quality
+          photos that improve your business image.
+        </p>
+      </div>
+    </article>
+  );
+};
+
+export default ArticleGraphic;
